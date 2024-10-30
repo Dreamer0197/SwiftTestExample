0dd636749168d2612ffef992bd65e69416bb5c8b
Kaan Şengün <427642@ogr.ktu.edu.tr>
2024-10-30 13:07:49 +0300

    Initial Commit

---
+++ SwiftTestExample/SwiftTestExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestExampleApp.swift
+//  SwiftTestExample
+//
+//  Created by Kaan Şengün on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTestExampleTests/SwiftTestExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestExampleTests.swift
+//  SwiftTestExampleTests
+//
+//  Created by Kaan Şengün on 30.10.2024.
+//
+
+import Testing
+@testable import SwiftTestExample
+
+struct SwiftTestExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
